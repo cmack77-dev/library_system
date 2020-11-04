98440dea62c1e2ae663188650c93fc465391430d
Stefan Kusmuk <stefan.kusmuk@hotmail.com>
2020-11-04 14:01:07 +0100

    shell for library management

--- src/JavaLibrarySystem.java
+++ src/JavaLibrarySystem.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
 
 public class JavaLibrarySystem {
+	
+	 enum Command {
+			
+			LIST,
+			CHECKOUT,
+			CHECKIN,
+			REGISTER,
+			DEREGISTER,
+			INFO,
+			QUIT,
+			UNKNOWN,
+		}
+
 
 	public static void main(String[] args) {
 		System.out.println("Welcome!");
+		
+		JavaLibrarySystem libraryManager = new JavaLibrarySystem();
+		JavaLibrarySystem.startSystem();
+		/* löses efter hand vi får pli på resten*/
 		
 
 
@@ -15,6 +33,111 @@ System.out.println(customer);
 
 movie.setBorrowedBy(customer);
 System.out.println(movie);
+
+	}
+		public void startSystem() {
+			
+			boolean running = true;
+			Scanner scanner = new Scanner(System.in); 
+			
+			while (running) {
+				
+				String userInput = scanner.nextLine();
+				Command command = parseCommand(userInput);
+				
+				if (command == Command.QUIT) {
+					running = false;
+					System.out.println("Exiting library.");
+					continue;
+					
+				} else if (command == Command.UNKNOWN) {
+					System.out.println("Unknown command. Try again");
+					continue;
+				}
+				
+	            String[] arguments = parseArguments(userInput);
+				
+				switch (command) {
+				case LIST:
+					listCommand(arguments);
+					break;
+				case CHECKOUT:
+					checkOutCommand(arguments);
+					break;
+				case CHECKIN:
+					checkInCommand(arguments);
+					break;
+				case REGISTER:
+					registerCommand(arguments);
+					break;
+				case DEREGISTER:
+					deRegisterCommand(arguments);
+					break;
+				case INFO:
+					infoCommand(arguments);
+					break;
+				}
+				
+			}
+			
+			scanner.close();
+				
+		}
+		
+		
+		private void listCommand() {
+			System.out.println(" /* listnamn */ ");
+		}
+		private void checkOutCommand(String[] arguments) {
+			
+		}
+		private void checkInCommand(String[] arguments) {
+		
+		}
+		private void registerCommand(String[] arguments) {
+			Scanner userReg = new Scanner(System.in); 
+			System.out.println("What do you want to register? A - Movie B - Book");
+			 /* bygger vidare på det nu*/
+			
+		}
+		private void deRegisterCommand(String[] arguments) {
+			
+		}
+		private void infoCommand() {
+			
+		}
+
+		
+		
+			private Command parseCommand(String userInput) {
+		    	String commandString = userInput.split(" ")[0];
+		    	switch(commandString) {
+		    		case "list":
+						return Command.LIST;
+		    		case "checkout":
+		    			return Command.CHECKOUT;
+		    		case "checkin":
+						return Command.CHECKIN;
+		    		case "register":
+		    			return Command.REGISTER;
+		    		case "deregister":
+		    			return Command.DEREGISTER;
+		    		case "info":
+						return Command.INFO;
+		    		case "quit":
+						return Command.QUIT;
+					default:
+						return Command.UNKNOWN;
+		    	}	
+				
+			}
+
+			
+			
+			
+			
+			
+			
 
 	}
 
